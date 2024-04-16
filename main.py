f6351ffb274a6a7043c9a2331db750b04e3128cf
Destiny <141277898+DestinyOnGit@users.noreply.github.com>
2024-04-16 11:47:14 -0700

    OH FUCK I DIDNT CHANGE THE VERSION

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from BRAPIF import *
 
 
 # Setup variables
-version: str = "C20"  # String, This is equivalent to 3.__ fyi
+version: str = "C21"  # String, This is equivalent to 3.__ fyi
 
 # Important variables
 _cwd = os.path.dirname(os.path.realpath(__file__))  # File Path
